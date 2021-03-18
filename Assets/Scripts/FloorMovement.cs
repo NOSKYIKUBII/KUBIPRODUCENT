93bb71e8a8bd8d3e0d87f6be58f4884af2aaba75
OlivierGawronek <olivieirgaw@gmail.com>
2021-03-18 21:30:42 +0100

    Dzien1
    Dodano podstawowe rzeczy i mechaniki.

---
+++ Assets/Scripts/FloorMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorMovement : MonoBehaviour
+{
+    public GameObject floorTile1;      //lewy
+    public GameObject floorTile2;      //prawy
+
+    public GameObject Tile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        floorTile1.transform.position -= new Vector3(10f * Time.deltaTime, 0f, 0f);
+        floorTile2.transform.position -= new Vector3(10f * Time.deltaTime, 0f, 0f);
+
+        if (floorTile2.transform.position.x < -1f)
+        {
+            floorTile1.transform.position += new Vector3(34.52f, 0f, 0f);
+
+            var newTile = Instantiate(Tile, floorTile2.transform.position + new Vector3(14.12f, 0, 0), Quaternion.identity);
+
+            Destroy(floorTile1);
+
+            floorTile1 = floorTile2;
+            floorTile2 = newTile;
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+
+    public float gravity;
+    public float jumpForce;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        transform.Rotate(new Vector3(0f, -10f * Time.deltaTime * 0f));
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.velocity = new Vector2(0f, jumpForce);
+        }
+
+
+        rigidbody.AddForce(new Vector2(0f, gravity * Time.deltaTime));
+    }
+
+
+
+}
